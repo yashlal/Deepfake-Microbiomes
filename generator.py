79029e75a978768c89e474bb093d0b1926fb9465
yashlal <yash.lal.m@gmail.com>
2021-05-01 12:13:49 -0700

    Commenting

--- generator.py
+++ generator.py
@@ -3,6 +3,7 @@ import numpy as np
 from predict_by_model import *
 import random as rd
 
+# Generates a random value in a Bernoulli style, used to determine whether or not a species will be included in the list
 def genrand(p):
     v = rd.random()
     if v <= p:
@@ -10,6 +11,7 @@ def genrand(p):
     else:
         return 0
 
+# Generates a random interaction matrix with 1s along the diagonal and a_ij = 1-a_ji property
 def generate_matrix(excel, sheetname, output_file_name):
     df = pd.read_excel(excel, sheet_name=sheetname, index_col=0)
     labels = df.index.to_list()
@@ -26,6 +28,7 @@ def generate_matrix(excel, sheetname, output_file_name):
         i += 1
     df.to_excel(output_file_name)
 
+#Takes a workbook, gets all the species, generates a random prob distro, then uses that to generate a species list and uses predict_community to predict the equilibrium. For testing, all probabilities are set to 1
 def generator_fxn(workbook, sheetname, n, pairwise_file):
     print('Reading excel...')
     df = pd.read_excel(workbook, sheet_name = sheetname, index_col = 0)
@@ -49,4 +52,6 @@ def generator_fxn(workbook, sheetname, n, pairwise_file):
         CommunityEquilibrium[j] = dict([(ky,val.round(3)) for ky,val in Equilibrium.items()])
     return CommunityEquilibrium
 
-print(generator_fxn('NewPW.xlsx', 'Relative_Abundance', 1, 'NewPW.xlsx'))
+#Run the generator
+if __name__ == '__main__':
+    print(generator_fxn('NewPW.xlsx', 'Relative_Abundance', 1, 'NewPW.xlsx'))

2ebcea1f8abf2d614dadae9f3cd0e523088a1ba4
yashlal <yash.lal.m@gmail.com>
2021-06-23 22:14:41 -0700

    Pre-New Feature

--- jacoboptim.py
+++ jacoboptim.py
@@ -7,6 +7,7 @@ from GenerateLambdas import *
 from autograd.builtins import tuple
 from scipy.integrate import odeint as BlackBox
 import matplotlib.pyplot as plt
+import math
 
 epoch = 1
 RADF = pd.read_excel('JacobTest.xlsx', index_col=0)
@@ -45,21 +46,27 @@ time = np.linspace(0,50,500)
 
 
 loss_values = []
+step_values = []
+learning_rate = 1
 while epoch<=1000:
     sol = BlackBox(odeSys, y0 = init_z, t = time, args = tuple([init_lam]))
     comm = anp.array([x.round(7) for x in sol[-1]])
     dist_grad = C_grad(comm, target_vector)
     step = sensitivity@dist_grad
+    step_values.append(anp.linalg.norm(step))
     dist = cost(comm, target_vector)
     loss_values.append(dist)
     print('Epoch Number ' + str(epoch) + ':' + str(dist))
-    print(comm)
+    # print(comm)
 
-    init_lam -= step
+    init_lam -= learning_rate*step
 
     sensitivity = anp.zeros((3,3,3))
 
     epoch += 1
 
-plt.plot(loss_values)
+# plt.plot(loss_values)
+step_values = [x for x in step_values if np.isnan(x)==False]
+plt.plot(step_values[0:-3])
 plt.show()
+print(step_values)

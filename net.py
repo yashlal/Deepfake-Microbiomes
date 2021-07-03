c5e98c1f36cac399787e6401f341544d126ef73c
ponte-vecchio <64239070+ponte-vecchio@users.noreply.github.com>
2021-07-04 00:35:04 +1000

    amend net.py with CUDA functionality

--- net.py
+++ net.py
@@ -5,60 +5,66 @@ import pickle
 import numpy as np
 import torch.optim as optim
 import time
+from math import sqrt
 
-device = torch.device("cuda" if use_cuda else "cpu")
+# select CUDA if available
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+if str(device) == 'cuda:0':
+	print('CUDA device selected!')
+elif str(device) == 'cpu':
+	print('CUDA device not available. CPU selected')
 
 class MyNet(nn.Module):
     def __init__(self):
         super(MyNet, self).__init__()
-        self.fc1 = nn.Linear(462, 462)
-        self.fc2 = nn.Linear(462, 462*5)
-        self.fc3 = nn.Linear(462*5, 231*461)
+        self.fc1 = nn.Linear(462, 462*5)
+        self.fc2 = nn.Linear(462*5, 462*10)
+        self.fc3 = nn.Linear(462*10, 462*20)
+        self.fc4 = nn.Linear(462*20, 231*461)
+        self.dropout = nn.Dropout(0.1)
     def forward(self,x):
         x = F.relu(self.fc1(x))
+        x = self.dropout(x)
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.dropout(x)
+       	x = F.relu(self.fc3(x))
+        x = self.dropout(x)
+        x = self.fc4(x)
         return x
 
 net = MyNet().to(device)
 
+
 # for n in range(1,151):
 #     PATH = 'Data/Train1-150/' + str(n)
-f = open('Data/Train1-150/1', 'rb')
+f = open('Data/Train1/1', 'rb')
 data = pickle.load(f)
 
-criterion = nn.MSELoss(reduction='sum')
-optimizer = optim.SGD(net.parameters(), lr=0.01)
+criterion = nn.MSELoss(reduction='mean')
+optimizer = optim.Adam(net.parameters(), lr=1e-4)
 
-for i in range(100):
-    start = time.time()
-    print(i)
 
+for i in range(1000):
     optimizer.zero_grad()
+    start = time.time()
 
     t1=time.time()
     input = torch.from_numpy(data[i][1]).float().to(device)
-    true_y = torch.FloatTensor(data[i][0])
-    print(time.time()-t1)
+    true_y = torch.FloatTensor(data[i][0]).to(device)
 
     t2=time.time()
-    output = net(input)
-    print(time.time()-t2)
+    output = net(input).to(device)
 
     t3=time.time()
-    loss = criterion(output, true_y)
-    print(loss)
-    print(time.time()-t3)
+    loss = criterion(output, true_y).to(device)
 
     t4=time.time()
     loss.backward()
-    print(time.time()-t4)
 
     t5=time.time()
     optimizer.step()
-    print(time.time()-t5)
 
-    print(time.time()-start)
+    print(f'Epoch {i}: {sqrt(loss.item())}')
 
 # PATH = 'model.pth'
 # torch.save(net.state_dict(), PATH)

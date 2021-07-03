5bd45d985fea18caa9548d189dd1fb5270a4384f
yashlal <yash.lal.m@gmail.com>
2021-07-03 15:51:11 -0700

    adding ipynb

--- net.py
+++ net.py
@@ -6,19 +6,21 @@ import numpy as np
 import torch.optim as optim
 import time
 
+device = torch.device("cuda" if use_cuda else "cpu")
+
 class MyNet(nn.Module):
     def __init__(self):
         super(MyNet, self).__init__()
-        self.fc1 = nn.Linear(462, 462*25)
-        self.fc2 = nn.Linear(462*25, 462*55)
-        self.fc3 = nn.Linear(462*55, 231*461)
+        self.fc1 = nn.Linear(462, 462)
+        self.fc2 = nn.Linear(462, 462*5)
+        self.fc3 = nn.Linear(462*5, 231*461)
     def forward(self,x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
-net = MyNet()
+net = MyNet().to(device)
 
 # for n in range(1,151):
 #     PATH = 'Data/Train1-150/' + str(n)
@@ -28,13 +30,14 @@ data = pickle.load(f)
 criterion = nn.MSELoss(reduction='sum')
 optimizer = optim.SGD(net.parameters(), lr=0.01)
 
-
 for i in range(100):
     start = time.time()
     print(i)
 
+    optimizer.zero_grad()
+
     t1=time.time()
-    input = torch.from_numpy(data[i][1]).float()
+    input = torch.from_numpy(data[i][1]).float().to(device)
     true_y = torch.FloatTensor(data[i][0])
     print(time.time()-t1)
 

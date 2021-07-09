d2a05a184a27f0266d327acd1f0e8be389410f18
yashlal <yash.lal.m@gmail.com>
2021-07-08 23:08:09 -0700

    FIxed Lambdas Training

---
+++ architecture.py
@@ -0,0 +1,125 @@
+from newsolver import predict_community_fullnp
+import numpy as np
+import pandas as pd
+import random as rd
+from numba import njit
+from numba.typed import List
+import pickle
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import torch.optim as optim
+import time
+from math import sqrt
+import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+from modules import regenerate_PWMatrix
+from scipy.stats import wasserstein_distance as WD
+
+data = pd.read_excel('RealData.xlsx', index_col=0)
+specs = data.columns.tolist()
+trimmed_specs = []
+typed_trimmed_specs = List()
+
+for i in range(len(specs)):
+    if data.iloc[:,i].astype(bool).sum() >= 85:
+        trimmed_specs.append(specs[i])
+        typed_trimmed_specs.append(specs[i])
+
+@njit()
+def get_LT(full_ar):
+    ar = []
+    for i in range(len(full_ar)):
+        for j in range(i):
+            ar.append(full_ar[i][j])
+    return ar
+
+@njit()
+def generate_matrix(comm):
+    dim = len(comm)
+    ar = np.zeros((dim,dim))
+
+    for i in range(dim):
+        for j in range(i+1):
+            if i == j:
+                ar[i][j] = 0
+            else:
+                r = rd.random()
+                ar[i][j] = r
+                ar[j][i] = 1-r
+
+    return ar
+
+def generate_train_set(n):
+    train_x = []
+    full_m = pd.DataFrame(generate_matrix(trimmed_specs), index=trimmed_specs, columns=trimmed_specs)
+    train_y = get_LT(full_m.to_numpy())
+
+    for epoch in range(n):
+
+        npcm = np.zeros(len(trimmed_specs))
+        size = rd.randint(25, 235)
+        subset = rd.sample(trimmed_specs, size)
+        subset_lam = (full_m.loc[subset, subset]).to_numpy()
+        cm = predict_community_fullnp(subset_lam, subset, verb=False)
+
+        for i in subset:
+            npcm[trimmed_specs.index(i)] = 0
+        for i in range(len(cm)):
+            npcm[trimmed_specs.index(subset[i])] = cm[i]
+        train_x.append(npcm)
+
+    return train_x, train_y
+
+# select CUDA if available
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+if str(device) == 'cuda:0':
+    print('CUDA device selected!')
+elif str(device) == 'cpu':
+	print('CUDA device not available. CPU selected')
+
+
+class MyNet(nn.Module):
+    def __init__(self, hyperparam):
+        super(MyNet, self).__init__()
+        self.fc1 = nn.Linear(462, hyperparam)
+        self.fc2 = nn.Linear(hyperparam, 231*461)
+    def forward(self,x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+def train_net(model, train_size):
+    pbar3=tqdm(range(train_size))
+    pbar3.set_description('Training Neural Net')
+    train_x, train_y = generate_train_set(n=train_size)
+
+    for i in pbar3:
+        optimizer.zero_grad()
+        x, y = train_x[i], train_y
+        input = torch.from_numpy(x).float().to(device)
+        true_y = torch.FloatTensor(y).to(device)
+        output = model(input).to(device)
+        loss = criterion(output, true_y).to(device)
+        s = sqrt(loss.item()/(231*461))
+        print(f'Epoch {i}: Loss {s}')
+        loss.backward()
+        optimizer.step()
+
+if __name__=='__main__':
+
+    train_size, test_size, param = 5000, 25, 500
+
+    net = MyNet(param).to(device)
+
+    #Multi GPU Support
+    if torch.cuda.device_count() > 1:
+          print(f'Using {torch.cuda.device_count()} GPUs')
+          net = nn.DataParallel(net)
+    elif torch.cuda.device_count() == 1:
+        print(f'Using {torch.cuda.device_count()} GPU')
+
+    criterion = nn.MSELoss(reduction='sum')
+    optimizer = optim.Adam(net.parameters(), lr=1e-4)
+    train_net(net, train_size=train_size)
